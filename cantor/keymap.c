2949d4e2204a2e6845b571076b9c694e9ec27e49
Slava <slava.istomin@tuta.io>
2025-04-24 16:43:13 +0500

    align all the layer schemes. Both docs and code

--- cantor/keymap.c
+++ cantor/keymap.c
@@ -323,18 +323,18 @@ void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
  *
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [L_BOO] = LAYOUT_split_3x6_3(/* BOO LAYOUT
-       --- '   ,   u   c   v                        q   f   d   l   y   /
-       --- a   o   e   s   g                        b   n   t   r   i   -
-       ---     x   .   w   z                        p   h   m   k   j   ---
+  [L_BOO] = LAYOUT_split_3x6_3(/** BOO LAYOUT
+       __  '   ,   u   c   v                        q   f   d   l   y   /
+       __  a   o   e   s   g                        b   n   t   r   i   -
+       __      x   .   w   z                        p   h   m   k   j   __
                      MOUSE sft SYMBOLS          ret spc RU
-                             __  __  __     __  __  __
        */
            __ , KC_QUOT, KC_COMM,    KC_U,   KC_C,  KC_V,     KC_Q,  KC_F,  KC_D,  KC_L,  KC_Y,   KC_SLASH,
            __ ,    BH_A,    BH_O,    BH_E,   BH_S,  KC_G,     KC_B,  BH_N,  BH_T,  BH_R,  BH_I,   KC_MINUS,
        KK_NOOP,      XX,    KC_X,  KC_DOT,   KC_W,  KC_Z,     KC_P,  KC_H,  KC_M,  KC_K,  KC_J,   KK_NOOP,
 
-                          KK_MOUSE , KK_SHIFT , KK_SYMBO,     KK_ENTER , KK_SPACE, KK_RU          ),
+                          KK_MOUSE , KK_SHIFT , KK_SYMBO,     KK_ENTER , KK_SPACE, KK_RU
+  ),
 
   /**
    * Russian layer.
@@ -345,15 +345,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * 2. Tap of Esc swithes back to English. This is especially useful in Vim
    * 3. When Ru is active, tap-hold the RU key. While you hold it, type English letters. Release to continue typing Russian. The idea is to facilitate typing English words in a flow of Russian text.
    */
-  [L_RUSSIAN] = LAYOUT_split_3x6_3(
-      // ёйцуке нгшщзх
-      //  фывап ролджэ
-      //  ячсми тьбю.
+  [L_RUSSIAN] = LAYOUT_split_3x6_3(/** Russian layer ```
+       ё   й   ц   у   к   е                        н   г   ш   щ   з   х
+       __  ф   ы   в   а   п                        р   о   л   д   ж   э
+       __  я   ч   с   м   и                        т   ь   б   ю   .   ъ
+                            __  __  __    __  __  __
+       ```
+       */
          RU_YO,   RU_Y,    RU_TS,    RU_U,   RU_K,  RU_E,     RU_N,  RU_G,   RU_SH, RU_SHCH,RU_Z,   RU_H,
            __ ,   RH_F,    RH_Y,     RH_V,   RH_A,  RU_P,     RU_R,  RH_O,   RH_L,  RH_D,   RH_Z,   RU_EE,
            __ ,   RU_YA,   RU_CH,    RU_S,   RU_M,  RU_I,     RU_T,  RU_SOFT,RU_B,  RU_YU,  RU_DOT, RU_HARD,
-
-                         __     ,         __  ,    __  ,       __  ,    __  ,    __                      ),
+                                     __ ,    __ ,   __ ,       __ ,   __ ,   __ ,
+  ),
 
   /**
    * Layer for symbols.
@@ -386,16 +389,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  - right hand: ringer finger: top row
    */
   [L_SYMBOLS] = LAYOUT_split_3x6_3(/*
-        __  `   &   *   /   __                       __  \   +   __  ~   __
-        __  ;   {   (   [   <                        >   ]   )   }   :   __
-        __  __  !   @   #   󰹿                        󰭜   |   ?   __  __  __
-                             __  __  __     __  __  __
+       __  `   &   *   /   __                       __  \   +   __  ~   __
+       __  ;   {   (   [   <                        >   ]   )   }   :   __
+       __  __  !   @   #   󰹿                        󰭜   |   ?   __  __  __
+                            __  __  __    __  __  __
        */
         XX,  KC_GRV,   KC_AMPR,  KC_ASTR, KC_SLSH,      XX,       XX,      KC_BSLS,  KC_PLUS,  KC_PIPE, KC_TILD, __,
         XX, KC_SCLN,   KC_LCBR,  KC_LPRN, KC_LBRC, KC_LABK,       KC_RABK, KC_RBRC,  KC_RPRN,  KC_RCBR, KC_COLN, __,
         XX,      XX,   KC_EXLM,    KC_AT, KC_HASH,  KC_DEL,       KC_BSPC,  KC_EQL,  KC_RABK,  KC_QUES, XX,      XX,
-
-                                      __,      __,     __,        __,       __,      __                  ),
+                                       __ ,    __ ,   __ ,         __ ,   __ ,   __ ,
+  ),
 
   /**
    * Layer for numbers and navigation.
@@ -410,16 +413,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * - `/` `:` `.` is to type `05/06/1970` `05:50` `3.1415`
    */
   [L_NUM_NAV] = LAYOUT_split_3x6_3(/*
-        __  __  7   8   9   /                        __  pg↑ ↑   pg↓ __  __
-        __  0   4   5   6   :                        ⇤-  ←   ⏎   →   -⇥  __
-        __  0   1   2   3   .                        __  ⮀   ↓   __  __  __
-                            ___ ___ ___     ___ ___ ___
+       __  __  7   8   9   /                        __  pg↑ ↑   pg↓ __  __
+       __  0   4   5   6   :                        ⇤-  ←   ⏎   →   -⇥  __
+       __  0   1   2   3   .                        __  ⮀   ↓   __  __  __
+                            __  __  __    __  __  __
        */
-        XX,      XX,      KC_7,     KC_8,    KC_9, KC_SLSH,       XX,      KC_PGUP,  KC_UP,    KC_PGDN, XX,      XX,
-        XX,    KC_0,      KC_4,     KC_5,    KC_6, KC_COLN,       KC_HOME, KC_LEFT,  KC_ENTER, KC_RGHT, KC_END,  XX,
-        XX,    KC_0,      KC_1,     KC_2,    KC_3,  KC_DOT,       XX,      KC_TAB,   KC_DOWN,  XX,      XX,      XX,
-
-                         __     ,         __  ,    __  ,       __  ,    __  ,    __                      ),
+        XX,    XX,  KC_7,  KC_8,  KC_9, KC_SLSH,       XX,      KC_PGUP,  KC_UP,    KC_PGDN, XX,      XX,
+        XX,  KC_0,  KC_4,  KC_5,  KC_6, KC_COLN,       KC_HOME, KC_LEFT,  KC_ENTER, KC_RGHT, KC_END,  XX,
+        XX,  KC_0,  KC_1,  KC_2,  KC_3,  KC_DOT,       XX,      KC_TAB,   KC_DOWN,  XX,      XX,      XX,
+                            __ ,    __ ,   __ ,         __ ,   __ ,   __ ,
+  ),
   /**
    * Layer for F keys and multimedia buttons.
    *
@@ -434,13 +437,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         __ F11  F7  F8  F9  UVIM                     __  br↑ vl↑ ULX __  __
         __ F11  F4  F5  F6  __                       __  __  mut __  __  __
         __ F10  F1  F2  F3  __                       __  br↓ vl↓ __  __  __
-                             __ __ __         __ __ __
+                             __  __  __     __  __  __
        */
-        XX,  KC_F12,     KC_F7,    KC_F8,   KC_F9, UC_VIM,       XX, KC_BRIU,  KC_VOLU,  UC_LINX,      XX,      XX,
-        XX,  KC_F11,     KC_F4,    KC_F5,   KC_F6,     XX,       XX,      XX,  KC_MUTE,       XX,      XX,      XX,
-   QK_BOOT,  KC_F10,     KC_F1,    KC_F2,   KC_F3,     XX,       XX, KC_BRID,  KC_VOLD,       XX,      XX, QK_BOOT,
-
-                         __     ,         __  ,    __  ,       __  ,    __  ,    __                      ),
+        XX,  KC_F12,  KC_F7,  KC_F8,  KC_F9, UC_VIM,       XX, KC_BRIU,  KC_VOLU,  UC_LINX,  XX,      XX,
+        XX,  KC_F11,  KC_F4,  KC_F5,  KC_F6,     XX,       XX,      XX,  KC_MUTE,       XX,  XX,      XX,
+   QK_BOOT,  KC_F10,  KC_F1,  KC_F2,  KC_F3,     XX,       XX, KC_BRID,  KC_VOLD,       XX,  XX, QK_BOOT,
+                                __ ,    __ ,   __ ,         __ ,   __ ,   __ ,
+  ),
 
   /**
    * Mouse layer.
@@ -455,23 +458,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Idea: my dream is to implement mouse bisection (with every step you choose direction to finally get the destination point. Like in binary search.). It's even possible and easy with [digitizer](https://docs.qmk.fm/features/digitizer). But unfortunately digitizer doesn't work on my Linux.
    */
   [L_MOUSE] = LAYOUT_split_3x6_3(/*
-        __ __  __  __  __  __                       __  w↑  ↑  w↓  b3  __
-        __ sft alt b1  ctl __                       __  <-  b1 ->  b2  __
-        __ __  __  __  __  __                       __  __  ↓  __  __  __
-                          ___  ___ ___     ___ ___  ___
+        __  __  __  __  __  __                       __  w↑  ↑  w↓  b3  __
+        __  sft alt b1  ctl __                       __  <-  c  ->  b2  __
+        __  __  __  __  __  __                       __  __  ↓  __  __  __
+                             __  __  __     __  __  __
        */
-        XX,      XX,        XX,       XX,      XX,     XX,       XX, KC_WH_U,  KC_MS_U,  KC_WH_D, KC_BTN3,      XX,
-        XX, KC_LSFT,   KC_LALT,  KC_BTN1, KC_LCTL,     XX,       XX, KC_MS_L,  KC_BTN1,  KC_MS_R, KC_BTN2,      XX,
-        XX,      XX,        XX,       XX,      XX,     XX,       XX, KK_CSB1,  KC_MS_D,       XX,      XX,      XX,
-
-                         __     ,         __  ,    __  ,       __  ,    __  ,    __                      ),
+        XX,      XX,        XX,       XX,      XX,  XX,       XX, KC_WH_U,  KC_MS_U,  KC_WH_D, KC_BTN3,  XX,
+        XX, KC_LSFT,   KC_LALT,  KC_BTN1, KC_LCTL,  XX,       XX, KC_MS_L,  KC_BTN1,  KC_MS_R, KC_BTN2,  XX,
+        XX,      XX,        XX,       XX,      XX,  XX,       XX, KK_CSB1,  KC_MS_D,       XX,      XX,  XX,
+                                   __ ,    __ ,   __ ,        __ ,   __ ,   __ ,
+  ),
 };
 
 /*
- MAYBE make a shared layout for cantor and dactyl
+ mayBE make a shared layout for cantor and dactyl
  make a macro to convert cantor's layer to a dactyl's
 */
 
-/* Facility:
+/* facility:
  * , and . are used only as non-shifted. There are two half-keys to occuqy!
  */
