000b6971c6b8d440797629710837c22df89d88b0
Slava <slava.istomin@tuta.io>
2025-04-28 16:09:59 +0500

    cantor: save bits of spase

--- cantor/rules.mk
+++ cantor/rules.mk
@@ -1,5 +1,10 @@
-LTO_ENABLE = yes # better size of firmware
+LTO_ENABLE = yes # better size of firmware (link time optimization)
 CONSOLE_ENABLE = yes
+
+# save a bit of space
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
 
 MOUSEKEY_ENABLE = yes      # Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
